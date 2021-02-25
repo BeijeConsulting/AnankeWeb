2326e88acaff2e26cef0427f108ae4f298582c45
mirabellomusic <giannimir@live.it>
2021-02-25 16:41:44 +0100

    done

--- Javascript/Foglio Excel/functions.js
+++ Javascript/Foglio Excel/functions.js
@@ -1,6 +1,8 @@
 
-var container = document.getElementById('containerTable');
 
+var container = document.getElementById('containerTable');
+let a=1;
+let b=1;
 
 createTable();
 initFocus();
@@ -29,8 +31,7 @@ console.log(row);
 var input = document.getElementById('1x1').focus();
 }
 
-let a=1;
-let b=1;
+
 function initFocus(){
     document.getElementById(a + "x" + b).focus();
 }
@@ -38,8 +39,34 @@ function initFocus(){
 function moveInput(event){
     var x = event.keyCode;
     console.log(x);
-    if(x === '37'){
-        getElementById("")
+    if(x === 37){
+       if(b != 1){
+           b--;
+       } else{
+          if(a!=1){
+              a--;
+              b=30;
+          } 
+       }
+        
+    } else if(x === 38 && a != 1){
+        a--;
+        
+    } else if( x === 39){
+        if(b === 30){
+            if(a != 5){
+                a++;
+                b=1;
+            }
+        }else{
+            b++;
+        }
+    } else if(x === 40){
+        if(a != 5){
+            a++;
+        }
     }
+    document.getElementById(a + "x" +b).focus();
 }
+
 
