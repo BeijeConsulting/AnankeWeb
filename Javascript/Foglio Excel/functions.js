5e678a4ef7ec4a7ca6c3faa53f1e795eb9b87905
mirabellomusic <giannimir@live.it>
2021-02-25 15:35:57 +0100

    Update ID RxC

--- Javascript/Foglio Excel/functions.js
+++ Javascript/Foglio Excel/functions.js
@@ -1,6 +1,7 @@
 
 var container = document.getElementById('containerTable');
-
+var button = document.getElementById('buttonClick');
+var buttonSubmit = document.getElementById('buttonSubmit');
 createTable();
 
 function createTable(){
@@ -11,12 +12,12 @@ function createTable(){
         row.setAttribute("id","row" +j);
         for(i=1;i<=30;i++){
     var div = document.createElement("div");
-    div.setAttribute("id","container"+j +""+ i);
+    div.setAttribute("id","container"+j +"x"+ i);
     var cell = document.createElement("input");
     cell.setAttribute("type","text");
-    cell.setAttribute("id",j +""+ i);
-    cell.setAttribute("name",j +""+ i);
-    cell.setAttribute("placeholder",j +""+ i);
+    cell.setAttribute("id",j +"x"+ i);
+    cell.setAttribute("name",j +"x"+ i);
+    cell.setAttribute("placeholder",j +"x"+ i);
     div.appendChild(cell);
     
     row.appendChild(div)
@@ -27,3 +28,15 @@ console.log(row);
 }
 }
 
+function getTable(){
+if(container.style.visibility == 'hidden'){
+    container.style.visibility = 'visible';
+    buttonSubmit.style.visibility = 'visible';
+    button.innerHTML= "Chiudi Tabella";
+
+}else{
+    container.style.visibility = 'hidden';
+    buttonSubmit.style.visibility = 'hidden';
+    button.innerHTML= "Apri Tabella";
+}
+}
